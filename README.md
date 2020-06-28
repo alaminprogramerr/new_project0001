dc8f80703e20ff8037644014686f7deb157318c4
Alamin <60449971+alaminprogramerr@users.noreply.github.com>
2020-06-28 04:50:05 -0500

    project started

---
+++ README.md
@@ -0,0 +1,6 @@
+# material_crud
+Dear . in this app  i useed reactjs , bootstrap , node js , matreial ui , and mongodb as database . 
+For run this app pls follow this page: https://blog.cloudboost.io/creating-your-first-mern-stack-application-b6604d12e4d3?gi=e4cae15e69da
+
+
+Thank you so much
---
+++ server.js
@@ -0,0 +1,27 @@
+const express=require('express')
+const app=express()
+const PORT=process.env.PORT||5000
+const mongoos=require('mongoose')
+const userRouter=require('./router/userRouter')
+const bodyParser=require('body-parser')
+const cors =require('cors')
+
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(bodyParser.json())
+
+mongoos.connect('mongodb://localhost/rakesh-app',{useFindAndModify:false,useUnifiedTopology:true,useNewUrlParser:true},(err=>{
+    if(err){
+        console.log(err)
+        return
+    }
+    console.log('Mongodb  connected')
+}))
+
+
+
+app.use(userRouter)
+app.listen(PORT,()=>{
+    console.log(`Server started on port `,PORT)
+})
